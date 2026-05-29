2b6e77c0a0fdc90fe8c71ca66f0e3ea540153e2b
Mykhailo Halai <mishagalay2@gmail.com>
2026-05-29 14:02:23 +0300

    fix: make HealthSchema attributes required

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
 class HealthSchema(BaseModel):
-    status_code: Optional[int] = None
-    detail: Optional[str] = None
-    result: Optional[str] = None
+    status_code: int
+    detail: str
+    result: str
