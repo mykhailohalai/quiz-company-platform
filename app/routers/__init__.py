85d4a0e84369d6b291894dd0d109a0d1f09a11b1
Mykhailo Halai <mishagalay2@gmail.com>
2026-05-30 13:23:15 +0300

    BE-3: refactor: replace 200 with status.HTTP_200_OK in all endpoints

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from redis.asyncio import Redis
@@ -12,18 +12,24 @@ router = APIRouter()
 
 @router.get("/", response_model=schemas.HealthSchema)
 async def health_check():
-    return schemas.HealthSchema(status_code=200, detail="ok", result="working")
+    return schemas.HealthSchema(
+        status_code=status.HTTP_200_OK, detail="ok", result="working"
+    )
 
 
 # check if db connected
 @router.get("/db-health", response_model=schemas.HealthSchema)
 async def db_health_check(db: AsyncSession = Depends(get_db)):
     await db.execute(text("SELECT 1"))
-    return schemas.HealthSchema(status_code=200, detail="ok", result="db connected")
+    return schemas.HealthSchema(
+        status_code=status.HTTP_200_OK, detail="ok", result="db connected"
+    )
 
 
 # check if redis connected
 @router.get("/redis-health", response_model=schemas.HealthSchema)
 async def redis_health_check(redis: Redis = Depends(get_redis)):
     await redis.ping()
-    return schemas.HealthSchema(status_code=200, detail="ok", result="redis connected")
+    return schemas.HealthSchema(
+        status_code=status.HTTP_200_OK, detail="ok", result="redis connected"
+    )
