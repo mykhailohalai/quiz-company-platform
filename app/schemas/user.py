63d8eee76b4f0511280dc822d9831af2243bec39
Mykhailo Halai <mishagalay2@gmail.com>
2026-06-05 15:02:37 +0300

    BE-5: refactor: update user schemas to match db constraints

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,31 +4,31 @@ from uuid import UUID, uuid4
 
 
 class UserSignInRequestSchema(BaseModel):
-    username: str
-    password: str
-    email: EmailStr | None = None
+    username: str = Field(..., min_length=1, max_length=50)
+    password: str = Field(..., min_length=8, max_length=255)
+    email: EmailStr | None = Field(default=None, max_length=100)
 
 
 class UserSignUpRequestSchema(BaseModel):
-    fname: str
-    sname: str
-    username: str
-    email: EmailStr | None = None
-    password: str = Field(min_length=8)
+    fname: str | None = Field(default=None, min_length=1, max_length=50)
+    sname: str | None = Field(default=None, min_length=1, max_length=50)
+    username: str = Field(..., min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=100)
+    password: str = Field(..., min_length=8, max_length=255)
 
 
 class UserUpdateRequestSchema(BaseModel):
-    fname: str | None = None
-    sname: str | None = None
-    username: str | None = None
-    email: EmailStr | None = None
-    password: str | None = Field(default=None, min_length=8)
+    fname: str | None = Field(default=None, min_length=1, max_length=50)
+    sname: str | None = Field(default=None, min_length=1, max_length=50)
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=100)
+    password: str | None = Field(default=None, min_length=8, max_length=255)
 
 
 class UserDetailResponseSchema(BaseModel):
     id: UUID
-    fname: str
-    sname: str
+    fname: str | None = None
+    sname: str | None = None
     username: str
     email: EmailStr | None = None
     created_at: datetime
