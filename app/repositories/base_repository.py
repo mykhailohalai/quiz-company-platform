d116c0bf65577b7c1423b38e787f1c144f2a7c43
Mykhailo Halai <mishagalay2@gmail.com>
2026-06-15 16:36:03 +0300

    BE-8: remove optional visibility on company create and guard delete against missing entity

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -49,6 +49,8 @@ class BaseRepository(Generic[EntityType, UpdateSchemaType]):
 
     async def delete(self, id: UUID) -> bool:
         entity = await self.get_by_id(id)
+        if entity is None:
+            raise self.not_found_exception(id)
         await self.session.delete(entity)
         return True
 
