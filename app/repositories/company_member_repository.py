567a31c561d461048a73906b9fd9b43b95f820e4
Mykhailo Halai <mishagalay2@gmail.com>
2026-06-16 15:47:39 +0300

    BE-9: add CompanyMemberRepository, schemas and basic exceptions

---
+++ app/repositories/company_member_repository.py
@@ -0,0 +1,88 @@
+from uuid import UUID
+
+from sqlalchemy import and_, func, select
+
+from app.models.user import User
+from app.repositories.base_repository import BaseRepository
+from app.models.company_member import CompanyMember, InviteStatus, Role
+
+
+class CompanyMemberRepository(BaseRepository[CompanyMember, BaseModel]):
+    model = CompanyMember
+    not_found_exception = CompanyMemberNotFoundException
+    already_exists_exception = CompanyMemberAlreadyExsitsException
+
+    async def get_by_company_and_user(self, company_id: UUID, user_id: UUID):
+        requests = await self.session.execute(
+            select(CompanyMember).where(
+                and_(
+                    company_id == CompanyMember.company_id
+                    and user_id == CompanyMember.user_id
+                )
+            )
+        )
+
+        return requests.scalar_one_or_none()
+
+    async def get_members_by_company(self, company_id: UUID, skip: int, limit: int):
+        total = await self.session.scalar(
+            select(func.count())
+            .select_from(CompanyMember)
+            .where(
+                and_(
+                    CompanyMember.status == InviteStatus.Active,
+                    CompanyMember.company_id == company_id,
+                )
+            )
+        )
+
+        result = await self.session.execute(
+            select(CompanyMember).where(
+                and_(
+                    CompanyMember.status == InviteStatus.Active,
+                    CompanyMember.company_id == company_id,
+                )
+            ).offset(skip).limit(limit)
+        )
+
+        return result.scalars().all(), total
+
+    # Owner invites user
+    async def get_invitation_by_user(self, user_id: UUID):
+        invatations = await self.session.execute(
+            select(CompanyMember).where(
+                and_(
+                    CompanyMember.status == InviteStatus.Pending_invite,
+                    CompanyMember.user_id == user_id
+                )
+            )
+        )
+
+        return invatations.scalars().all()
+
+    async def get_request_by_company(self, company_id: UUID):
+        requests = await self.session.execute(
+            select(CompanyMember).where(
+                and_(
+                    CompanyMember.status == InviteStatus.Pending_request,
+                    CompanyMember.company_id == company_id
+                )
+            )
+        )
+
+        return requests.scalars().all()
+
+    
+    # Invated by owner
+    async def get_invitations_by_company(self,company_id: UUID):
+        requests = await self.get_request_by_company(company_id)
+        users = await self.session.execute(
+            select(CompanyMember).where(
+                and_(
+                    CompanyMember.status == InviteStatus.Pending_invite,
+                    CompanyMember.company_id == company_id
+                )
+            )
+        )
+
+        return users.scalars().all()
